432a4817b840339bba479c7070556eb9b46f1ad5
Markadrian6399 <markodiba6499@gmail.com>
2023-09-22 02:59:18 -0700

    first commit

--- app/marketplace/imagecom/page.jsx
+++ app/marketplace/imagecom/page.jsx
@@ -1,13 +1,13 @@
 import ImageGallery from "@/app/components/ImageGallery";
 import React from "react";
 
-async function getData() {
-  const res = await fetch("http://localhost:8000/api/products");
-  if (!res.ok) {
-    throw new Error("Faild to fetch data");
-  }
-  return res.json();
-}
+// async function getData() {
+//   const res = await fetch("http://localhost:8000/api/products");
+//   if (!res.ok) {
+//     throw new Error("Faild to fetch data");
+//   }
+//   return res.json();
+// }
 
 const page = () => {
   const data = getData();
